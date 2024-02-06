182f65dfdb91f2749f9764ec2d7d008c331577c4
Harish Pal <palharish003@gmail.com>
2024-02-06 18:29:57 +0530

    HomeControllerAdded

---
+++ SevenMinutesBook_v1/SevenMinutesBook_V1.Server/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SevenMinutesBook_V1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
