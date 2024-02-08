52e2af38a0364146390a98f3afa8f38d3d2fbb52
Harish Pal <palharish003@gmail.com>
2024-02-08 17:23:52 +0530

    Changed Book Title Datatype in the Db Table and Scaffold

---
+++ SevenMinutesBook_v1/SevenMinutesBook_V1.Server/Dto/BookDto.cs
@@ -0,0 +1,22 @@
+﻿namespace SevenMinutesBook_V1.Server.Dto
+{
+    public class BookDto
+    {
+
+        public int BookId { get; set; }
+        public string? BookTitle { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int Edition { get; set; }
+        public string Language { get; set; } = null!;
+        public string Isbn { get; set; } = null!;
+        public int GenreId { get; set; }
+        public int SubGenreId { get; set; }
+        public int TotalPages { get; set; }
+        public string About { get; set; } = null!;
+        public int PublisherId { get; set; }
+
+
+
+    }
+}
